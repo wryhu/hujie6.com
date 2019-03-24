eabf9564083d4231f6f994abeb2490d2aa38b0b9
jet <736209298@qq.com>
2019-03-24 18:05:06 +0800

    test

--- MySite/views.py
+++ MySite/views.py
@@ -269,10 +269,9 @@ def wx(request):
                             "Content": u"hello",
                         }
                     }
-                    result = xmltodict.unparse(resp_dict.encode("utf8"))
-                    print(result)
+                    result = xmltodict.unparse(resp_dict)
                     encryp_test = WXBizMsgCrypt(settings.WX_TOKEN, settings.WX_AESK, settings.WX_APPID)
-                    ret, encrypt_xml = encryp_test.EncryptMsg(result, nonce)
+                    ret, encrypt_xml = encryp_test.EncryptMsg(result.encode("utf8"), nonce)
 
                     return HttpResponse(encrypt_xml)
 
